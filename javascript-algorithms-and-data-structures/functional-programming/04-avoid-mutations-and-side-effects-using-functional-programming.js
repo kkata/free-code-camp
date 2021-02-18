96f80ba5939d630f960fb8dd569ee5f77ff6562e
kkata <14849535+kkata@users.noreply.github.com>
2021-02-18 23:59:39 +0900

    WIP: functional-programming

---
+++ javascript-algorithms-and-data-structures/functional-programming/04-avoid-mutations-and-side-effects-using-functional-programming.js
@@ -0,0 +1,13 @@
+// The global variable
+var fixedValue = 4;
+
+function incrementer () {
+  // Only change code below this line
+
+  return fixedValue + 1
+
+  // Only change code above this line
+}
+
+incrementer();
+console.log(fixedValue)
